e6dd5ede49156d497e118760549e102bfc45fa1f
tarunwalia12345 <57227656+tarunwalia12345@users.noreply.github.com>
2021-10-05 09:51:17 +0530

    Robot Shooter

---
+++ Assets/Scripts/Enemy Scripts/CannonScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonScript : MonoBehaviour {
+
+	private Transform player;
+	public GameObject explosion;
+
+	void Awake () {
+		player = GameObject.Find ("Robot").transform;
+	}
+
+	void Update () {
+		if (player) {
+			transform.rotation = Quaternion.Slerp (transform.rotation, 
+				Quaternion.LookRotation(player.position - transform.position),
+				2f * Time.deltaTime);
+		}
+	}
+
+	void Damage() {
+		Instantiate (explosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+
+	void OnParticleCollision(GameObject target) {
+		if (target.name == "Muzzle") {
+			Damage ();
+		}
+	}
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/Missile Scripts/ExplosionScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionScript : MonoBehaviour {
+
+	// Use this for initialization
+void start() {
+		Destroy(gameObject, 1f);
+
+	}
+}
